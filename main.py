dc14c2e952024d7fd5cf0c2b20b3dd5efba953d9
Viktoriia <vikalog91@gmail.com>
2022-10-08 02:24:54 +0300

    2task is added

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, json
+from faker import Faker
+from pandas.io.json import to_json
+
+fake = Faker()
 
 app = Flask(__name__)
 
@@ -8,3 +12,12 @@ def requirements():
     with open('requirements.txt', 'r') as f:
         text = f.read()
         return f"<pre>{text}</pre><link rel =\"stylesheet\"href=\"content.html\">"
+
+
+@app.route('/generate-users/count=<int:num>', methods=['GET'])
+def gen_user(num):
+    emails = []
+    for i in range(num):
+        i = fake.email()
+        emails.append(i)
+    return render_template("table.html", data=emails)
